865664c105bdce3f4ed32c02e745d2b7aab302ba
Nursultan606 <nursultanmursaliev14@gmail.com>
2024-08-14 17:50:10 +0500

    Add files via upload
    первый тест  третьего месяца

---
+++ тест/app.js
@@ -0,0 +1,54 @@
+//Первое задание
+function onlyNumber(str) {
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+console.log(onlyNumber("12345"));
+console.log(onlyNumber("abc"));
+
+
+//Второе задание
+function Seconds() {
+  let count = 0;
+  let intervalId = setInterval(() => {
+    console.log("Прошла секунда");
+    count++;
+    if (count >= 20) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+Seconds();
+
+//Третье задание
+const count = () => {
+    let count = 1;
+    const interval = setInterval(() => {
+        console.log(count);
+        count++;
+        if (count > 10) {
+            clearInterval(interval);
+        }
+    }, 1000);
+};
+
+count();
+
+//Четвертое задание
+
+const block = document.querySelector(".block");
+
+block.addEventListener("click", () => {
+      if (block.classList.contains("secondColor")) {
+        block.classList.remove("secondColor");
+      } else {
+        block.classList.add("secondColor");
+      }
+});
+
+// Пятое задание
+// Не смог сделать пятое задание так как плохо еще понимаю как работать с JSON
+
+
+
